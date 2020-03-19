6eee97ba92e438827753f8b037de70b128700e88
yk85-hub <55225896+yk85-hub@users.noreply.github.com>
2020-03-19 22:51:22 +0300

    Update Homepage.js

--- src/Homepage.js
+++ src/Homepage.js
@@ -37,19 +37,7 @@ export default class extends React.Component {
       
     
       
-      
-      <Typography className={classes.root}>
-  <Link href="#" onClick={preventDefault}>
-    Link
-  </Link>
-  <Link href="#" onClick={preventDefault} color="inherit">
-    {'color="inherit"'}
-  </Link>
-  <Link href="#" onClick={preventDefault} variant="body2">
-    {'variant="body2"'}
-  </Link>
-</Typography>
- 
+     
       
   
       
