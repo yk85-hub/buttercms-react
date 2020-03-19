3187bb3391794067b11d516215369460195126c2
yk85-hub <55225896+yk85-hub@users.noreply.github.com>
2020-03-19 22:43:35 +0300

    Update Homepage.js

--- src/Homepage.js
+++ src/Homepage.js
@@ -35,43 +35,20 @@ export default class extends React.Component {
       
       
       
-      
-      /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Link from '@material-ui/core/Link';
-import Typography from '@material-ui/core/Typography';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    '& > * + *': {
-      marginLeft: theme.spacing(2),
-    },
-  },
-}));
-
-export default function Links() {
-  const classes = useStyles();
-  const preventDefault = event => event.preventDefault();
-
-  return (
-    <Typography className={classes.root}>
-      <Link href="/blog" onClick={preventDefault}>
-        Link1
-      </Link>
-      <Link href="#" onClick={preventDefault} color="inherit">
-        {'color="inherit"'}
-      </Link>
-      <Link href="#" onClick={preventDefault} variant="body2">
-        {'variant="body2"'}
-      </Link>
-    </Typography>
-  );
-}
-
+    
       
       
-      
+      <Typography className={classes.root}>
+  <Link href="/blog" onClick={preventDefault}>
+    Link
+  </Link>
+  <Link href="#" onClick={preventDefault} color="inherit">
+    {'color="inherit"'}
+  </Link>
+  <Link href="#" onClick={preventDefault} variant="body2">
+    {'variant="body2"'}
+  </Link>
+</Typography>
       
       
       
