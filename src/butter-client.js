776441978a57bf28c965bdd8eece6224c2c0456c
yk85-hub <55225896+yk85-hub@users.noreply.github.com>
2020-03-19 19:49:19 +0300

    Update butter-client.js

--- src/butter-client.js
+++ src/butter-client.js
@@ -1,5 +1,5 @@
 import Butter from 'buttercms'
 
-const butter = Butter('<ee4e7a6ec21334902d24cf8d8bb38589c20b06b9>')
+const butter = Butter('ee4e7a6ec21334902d24cf8d8bb38589c20b06b9')
 
 export default butter
